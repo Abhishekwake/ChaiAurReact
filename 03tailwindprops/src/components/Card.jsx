062855ad22774ad7a78959f86bb34a816759db01
Abhishek Wake <abhishekwake111@gmail.com>
2025-08-03 09:39:20 +0530

    Learn about tailwind and props in react

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Card({channel="defaultName"}) {
+    console.log(channel);
+  return (
+  
+        <div className="max-w-xs p-6 rounded-md shadow-md bg-black">
+        <img
+          src="https://plus.unsplash.com/premium_photo-1690552678496-fda53292def5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
+          alt=""
+          className="object-cover object-center w-full rounded-md h-72 bg-gray-500"
+        />
+        <div className="mt-6 mb-2">
+          <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">
+            {channel}
+          </span>
+          <h2 className="text-xl font-semibold tracking-wide">Lorem ipsum dolor</h2>
+        </div>
+        <p className="text-gray-300">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio tempora ipsum soluta
+          amet
+        </p>
+      </div>
+   
+  )
+}
+
+export default Card
+
+//rfce 
